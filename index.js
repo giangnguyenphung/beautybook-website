6f6dce451ae2c156022b57fa61d03cc8a757de95
Jan Nguyen <giang.nss.c2@gmail.com>
2020-10-29 13:08:14 -0700

    crop img

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const burger = document.querySelector('.burger');
+const navSlide = () => {
+  const burger = document.querySelector('#burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links')
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.getElementsByClassName.animation){
+        link.getElementsByClassName.animation = '';
+      } else {
+        link.getElementsByClassName.animation = `navLinkFade 0.5s ease forward' ${index / 7 + 0.3}s`
+      }
+    })
+    // burger animation
+    burger.classList.toggle('toggle')
+  })
+};
+
+
+navSlide();
